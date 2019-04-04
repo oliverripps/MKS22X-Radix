5a165c5cb03aab5fa5ca328d9c290f70868fbc2e
Oliver Ripps <oripps@stuy.edu>
2019-04-03 21:04:06 -0400

    finished radix draft and added in Kdriver

--- Radix.java
+++ Radix.java
@@ -39,16 +39,16 @@ public class Radix{
           }
         }
       }
-      
-      /*LinkedList() l = new LinkedList();
-      nums[i]=l;
-      l.add(data[i]);
-      l.get(i).setTal=data[i]%digits;*/
+      for(int l=0;l<20;l++){
+        nums.extend(buckets[l]);
+      }
     }
-
-
+    int index = 0;
+    while (nums.size() > 0){
+      data[index] = nums.removeFront();
+      index++;
+    }
   }
-
 
   private static int digitsIn(int num){
     num=Math.abs(num);
@@ -69,5 +69,40 @@ public class Radix{
     }
     return digitsIn(longest);
   }
+
+  public static void main(String[]args){
+        System.out.println("Size\t\tMax Value\tquick/builtin ratio ");
+        int[]MAX_LIST = {1000000000,500,10};
+        for(int MAX : MAX_LIST){
+          for(int size = 31250; size < 1000001; size*=2){
+            long qtime=0;
+            long btime=0;
+            //average of 5 sorts.
+            for(int trial = 0 ; trial <=5; trial++){
+              int []data1 = new int[size];
+              int []data2 = new int[size];
+              for(int i = 0; i < data1.length; i++){
+                data1[i] = (int)(Math.random()*MAX);
+                data2[i] = data1[i];
+              }
+              long t1,t2;
+              t1 = System.currentTimeMillis();
+              Radix.radixsort(data2);
+              t2 = System.currentTimeMillis();
+              qtime += t2 - t1;
+              t1 = System.currentTimeMillis();
+              Arrays.sort(data1);
+              t2 = System.currentTimeMillis();
+              btime+= t2 - t1;
+              if(!Arrays.equals(data1,data2)){
+                System.out.println("FAIL TO SORT!");
+                System.exit(0);
+              }
+            }
+            System.out.println(size +"\t\t"+MAX+"\t"+1.0*qtime/btime);
+          }
+          System.out.println();
+        }
+      }
 
 }
